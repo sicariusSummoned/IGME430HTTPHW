ec341ab1ced898e2b96cd40fe88c7b02ff1b17c6
James Taggart <jxt1607@g.rit.edu>
2017-08-31 15:00:49 -0400

    Added a comment to test auto-test feature

--- IGME430/Simple-HTTP-Assignment-Start/src/server.js
+++ IGME430/Simple-HTTP-Assignment-Start/src/server.js
@@ -40,3 +40,5 @@ const onRequest = (request, response) => {
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
+
+//Making a change here to test pushing and automatic CircleCI testing
